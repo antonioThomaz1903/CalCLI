fbeb8bc999183e0c9cb0db25524b8bf83e413359
antonioThomaz1903 <antoniotor987@gmail.com>
2025-04-21 04:30:19 -0400

    feat: add basic logic to show expression #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
-
+#include "../include/calcli/expression.h"
+#include <string>
+#include <iostream>
+#include <memory>
 
 int main(){
-    std::cout << "Oi\n";
+    auto leftOperand = std::make_unique<OperandNode>(3);
+    auto rightOperand1 = std::make_unique<OperandNode>(4);
+    auto rightOperand2 = std::make_unique<OperandNode>(5);
+    
+    auto multiplyNode = std::make_unique<OperatorNode>('*', std::move(rightOperand1), std::move(rightOperand2));
+    auto addNode = std::make_unique<OperatorNode>('+', std::move(leftOperand), std::move(multiplyNode));
+
+    std::cout << "Resultado: " << addNode->evaluate() << std::endl;
+    std::cout << "Expressão: " << addNode->toString() << std::endl;
+    
     return 0;
 }
